f200437fc8ac5794fb068c824fb2af071cfc69d9
CSHao2018 <34595596+CSHao2018@users.noreply.github.com>
2019-04-16 09:25:37 +0800

    v1.0

---
+++ files/index/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="index.html",d="generationDate",e=new Date(1555320005837.79),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="f513381ebd4d4a65ab1cdef00c9bd488",q="type",r="Axure:Page",s="name",t="index",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="74de3db235c3450f8ea1f5c1d9426890",Y="label",Z="friendlyType",ba="图片 ",bb="imageBox",bc="styleType",bd="visible",be=true,bf="75a91ee5b9d042cfa01b8d565fe289c0",bg="size",bh="width",bi=1920,bj="height",bk=6258,bl="imageOverrides",bm="images",bn="normal~",bo="images/index/u0.jpg",bp="masters",bq="objectPaths",br="74de3db235c3450f8ea1f5c1d9426890",bs="scriptId",bt="u0";
+return _creator();
+})());
